205b30b3a229bfd9d168229a42c9460bdd2ea604
Mindy Lou <mml234@cornell.edu>
2017-02-01 16:10:37 -0500

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Mindy Lou on 2/1/17.
+//  Copyright © 2017 Mindy Lou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
